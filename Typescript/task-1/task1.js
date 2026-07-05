54a1abd8635b651e0c85c2aeed8a5ddb5f0f0353
MaximNikolaevv <146669687+MaximNikolaevv@users.noreply.github.com>
2026-07-05 23:29:40 +0300

    Introduction to TypeScript
    Setup for typescript

---
+++ Typescript/task-1/task1.js
@@ -0,0 +1,8 @@
+function EvenSum(number) {
+    const IsEvenOrSum = number % 2 === 0 ? true : false;
+    return IsEvenOrSum;
+}
+console.log(EvenSum(4));
+export {};
+// npm run build
+//    npm start
---
+++ Typescript/task-1/task1.ts
@@ -0,0 +1,9 @@
+function EvenSum(number: number): boolean {
+
+    const IsEvenOrSum: boolean = number % 2 === 0 ? true : false;
+    return IsEvenOrSum;
+}
+
+console.log(EvenSum(7));
+
+// npx tsx TypeScript/task1.ts
