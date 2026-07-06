eb92b7a6c7a47364a0d911805c1ef1d33d54ee1d
MaximNikolaevv <146669687+MaximNikolaevv@users.noreply.github.com>
2026-07-06 12:27:02 +0300

    Task-2 Completed + setup for running typescript

---
+++ out/task-2.js
@@ -0,0 +1,7 @@
+"use strict";
+function formatPerson(person) {
+    const [name, age] = person;
+    console.log(`Name: ${name}, Age: ${age}`);
+}
+formatPerson(['Ivan', 20]);
+//# sourceMappingURL=task-2.js.map
