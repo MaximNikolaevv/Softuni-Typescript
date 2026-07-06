dd26fe9d89e741fa2a3f3d3c32c6a115327ed7eb
MaximNikolaevv <146669687+MaximNikolaevv@users.noreply.github.com>
2026-07-06 13:33:05 +0300

    task-4 done

---
+++ task-4.ts
@@ -0,0 +1,44 @@
+function SummarizePerson(id: number, firstName: string, lastName: string, age: number, middleName?: string, hobbies?: string[], workinfo?: [string, number]): void {
+
+
+    const PersonInfo: (string | number)[] = [id];
+
+    if (middleName) {
+        PersonInfo.push([firstName, middleName, lastName].join(' '));
+    } else {
+        PersonInfo.push([firstName, lastName].join(' '));
+    }
+
+
+    PersonInfo.push(age);
+
+
+
+    if (hobbies) {
+        PersonInfo.push(...hobbies);
+    } else {
+        PersonInfo.push('-');
+    }
+
+    if (hobbies && hobbies.length > 1) {
+
+        hobbies.forEach((hobby, index) => {
+            if (index === hobbies.length - 1) {
+                PersonInfo.push(hobby);
+            } else {
+                PersonInfo.push(hobby + ',');
+            }
+        });
+    }
+
+
+    if (workinfo) {
+        PersonInfo.push(`${workinfo[0]} -> ${workinfo[1]}`);
+    } else {
+        PersonInfo.push('-');
+    }
+
+    console.log(PersonInfo);
+}
+
+SummarizePerson(21, 'Joseph', 'Angler', 28);
