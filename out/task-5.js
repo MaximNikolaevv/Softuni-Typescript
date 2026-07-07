b0163199b578326fc531f77b36fb5eccff03f100
MaximNikolaevv <146669687+MaximNikolaevv@users.noreply.github.com>
2026-07-07 22:31:28 +0300

    task-5-Done

---
+++ out/task-5.js
@@ -0,0 +1,13 @@
+"use strict";
+function UnknownResponse(params) {
+    if (typeof params === 'object' && params !== null) {
+        if (typeof params.value === 'string') {
+            console.log(`${params.value}`);
+        }
+    }
+    else {
+        console.log("-");
+    }
+}
+UnknownResponse({ code: 200, text: 'Ok', value: [1, 2, 3] });
+//# sourceMappingURL=task-5.js.map
